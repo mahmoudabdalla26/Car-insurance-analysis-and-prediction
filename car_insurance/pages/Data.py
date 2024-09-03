dea4c36c79007fc734cda6870f3e8c2212615533
Mahmoud Abdalla <112134663+mahmoudabdalla26@users.noreply.github.com>
2024-09-04 02:24:56 +0300

    Update Data.py

--- car_insurance/pages/Data.py
+++ car_insurance/pages/Data.py
@@ -9,7 +9,7 @@ st.markdown("<center><h1> Car Insurance Claims Dataset </h1></center>", unsafe_a
 st.write('Car Insurance Claims Dataset Before Cleaning And Preprocessing:')
 
 # Reading and displaying the dataset before cleaning and preprocessing
-df_before = pd.read_csv(r"C:\Users\Lenovo\Desktop\final_project\car_insurance\Sourse\modified_train_subset.csv")
+df_before = pd.read_csv(r"car_insurance/Sourse/modified_train_subset.csv")
 st.write(df_before)
 
 # Displaying information about the dataset after cleaning and preprocessing
